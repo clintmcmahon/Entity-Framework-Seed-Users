368a9effe382b71ed5797976057d816e1c0f5087
Clint McMahon <clint.mcmahon@gmail.com>
2014-11-12 19:41:48 -0600

    Initial check-in

---
+++ Configuration.cs
@@ -0,0 +1,39 @@
+namespace EntityFramework
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            if (!context.Roles.Any(r => r.Name == "Administrator"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Administrator" };
+
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(u => u.UserName == "admin"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "admin" };
+
+                manager.Create(user, "@dm1n!");
+                manager.AddToRole(user.Id, "Administrator");
+            }
+        }
+    }
+}
